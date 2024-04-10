dbd1d34153e266577f539cf6ce6b7db597b18aab
gstwbtlf <166173171+gstwbtlf@users.noreply.github.com>
2024-04-10 19:08:39 -0400

    Added config file for sessions security
    Added config file for sessions security

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+
+ini_set('session.use_only_cookies', 1);
+
+// use session id only created by server within the website
+ini_set('session.use_strict_mode', 1);
+
+// cookie parameters when sessions start
+session_set_cookie_params([
+    //cookie gets destroyed after a certain amount of time
+    //30 minutes
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    //only run cookie in a secure website connection
+    'secure' => true,
+    'httponly' => true
+]);
+
+session_start();
+
+//first time session created
+if (!isset($_SESSION['last_regeneration'])){
+    
+    //generate new id for current session that is stronger
+    session_regenerate_id(true);
+    $_SESSION['last_regeneration'] = time();
+
+} else {
+
+    //regenerate session id
+    $interval = 60 * 30;
+
+    if (time() - $_SESSION['last_regeneration'] >= $interval){
+        $_SESSION['last_regeneration'] = time();
+    }
+}
+
